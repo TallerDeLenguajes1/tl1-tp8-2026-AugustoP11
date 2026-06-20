846f5cbe4425274b998c30499af9bbd5258b10de
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2026-06-19 21:04:24 -0300

    Ejercicio 1.3 completado

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -11,7 +11,9 @@ int contadorID = 0, duracionTarea;
 //Descripcion
 string descripcionTarea;
 
+//Creo las listas de tareas
 List<Tarea> tareasPendientes = new List<Tarea>();
+List<Tarea> tareasRealizadas = new List<Tarea>();
 
 //Carga de tareas a la lista de tareas pendientes
 for (int i = 0; i < numeroDeTareas; i++)
@@ -28,5 +30,48 @@ for (int i = 0; i < numeroDeTareas; i++)
     } while (duracionTarea < 10 || duracionTarea > 100);
 
     tareasPendientes.Add(new Tarea(contadorID, descripcionTarea, duracionTarea));
+
+    contadorID++;
 }
 
+
+//_______________________________________________________________
+//_______INTERFAZ PARA MOVER TAREAS PENDIENTES A REALIZADAS______
+//_______________________________________________________________
+
+int opcion, id;
+do
+{
+    Console.WriteLine("Ingrese '1' para marcar una tarea como realizada mediante ID");
+    Console.WriteLine("Ingrese '0' para salir");
+
+    opcion = int.Parse(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case 0:
+        break;
+
+        case 1:
+            do
+            {
+                Console.WriteLine("\nIngrese un ID de tarea valido:");
+                id = int.Parse(Console.ReadLine());
+            } while (id < 0 || id > numeroDeTareas);
+
+            foreach (var tarea in tareasPendientes)
+            {
+                if (id == tarea.TareaID)
+                {
+                    tareasRealizadas.Add(tarea);
+                    tareasPendientes.Remove(tarea);
+                    break;
+                }
+            }
+        break;
+
+        default:
+        break;
+    }
+
+} while (opcion != 0);
