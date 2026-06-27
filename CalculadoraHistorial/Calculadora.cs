3075f2ef7102dcf8d9cd8b74bb79cc031426cedc
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2026-06-27 11:58:21 -0300

    Agrego la nueva funcionalidad Historial

--- CalculadoraHistorial/Calculadora.cs
+++ CalculadoraHistorial/Calculadora.cs
@@ -1,3 +1,5 @@
+using EspacioOperacion;
+using EspacioTipoOperacion;
 namespace EspacioCalculadora;
 
 public class Calculadora
@@ -5,6 +7,8 @@ public class Calculadora
     private double dato;
 
     public double Dato{get => dato; set => dato = value;}
+
+    public List<Operacion> Historial = new List<Operacion>();
 
     public Calculadora()
     {
@@ -16,29 +20,38 @@ public class Calculadora
     public void Limpiar()
     {
         dato = 0;
+        Historial.Clear();
         Console.WriteLine("Memoria limpiada con exito!");
     }
 
     public void Sumar(double termino)
     {
+        double aux = dato;
         dato += termino;
+        Historial.Add(new Operacion(aux, dato, TipoOperacion.Suma)); 
     }
 
     public void Restar(double termino)
     {
+        double aux = dato;
         dato -= termino;
+        Historial.Add(new Operacion(aux, dato, TipoOperacion.Resta));
     }
 
     public void Multiplicar(double termino)
     {
+        double aux = dato;
         dato *= termino;
+        Historial.Add(new Operacion(aux, dato, TipoOperacion.Multiplicacion));
     }
 
     public void Dividir(double termino)
     {
         if (termino != 0)
         {
+            double aux = dato;
             dato /= termino;
+            Historial.Add(new Operacion(aux, dato, TipoOperacion.Division));
         } else
         {
             Console.WriteLine("No se puede dividir por 0!");
